af8be231d508e3f3dafd5cbf80c3eefd2298613e
Schalk Griebenow <schalkgriebenow@Scalk-Griebenow.local>
2025-06-10 07:41:29 +0200

    chore: remove old Drizzle migrations

--- src/db/migrations/0000_hard_virginia_dare.sql
+++
@@ -1,8 +0,0 @@
-CREATE TYPE "public"."status" AS ENUM('open', 'paid', 'void', 'uncollectable');--> statement-breakpoint
-CREATE TABLE "invoices" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"amount" integer NOT NULL,
-	"description" text NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	"status" "status" DEFAULT 'open' NOT NULL
-);
--- src/db/migrations/0002_yielding_jimmy_woo.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "invoices" DROP COLUMN "customer";--> statement-breakpoint
-ALTER TABLE "invoices" DROP COLUMN "email";
